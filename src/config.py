79f139416ee8ce79a32f43b7f382bc52071e6743
Weini Liu <weinliu@microsoft.com>
2018-07-11 15:09:34 -0700

    Fix email batch

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ DEBUG = True
 ADMINS = frozenset([
     os.environ.get('ADMIN_EMAIL')
 ])
-GMAIL = os.environ.get('ADMIN_EMAIL')
+GMAIL = os.environ.get('GMAIL')
 PASSWORD = os.environ.get('GMAIL_PASSWORD')
 DOMAIN = "https://whaleofdeal.herokuapp.com"
